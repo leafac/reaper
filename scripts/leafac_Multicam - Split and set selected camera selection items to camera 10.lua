94eb30755cb2f091952e990198a8496e518b1c82
Leandro Facchinetti <git@leafac.com>
2020-12-08 20:15:02 +0000

    .

---
+++ scripts/leafac_Multicam - Split and set selected camera selection items to camera 10.lua
@@ -0,0 +1,8 @@
+reaper.Main_OnCommand(40012, 0) -- Item: Split items at edit or play cursor
+local camera = string.match(select(2, reaper.get_action_context()),
+                            "(%d+)%.lua$")
+for index = 0, reaper.CountSelectedMediaItems(0) - 1 do
+    reaper.GetSetMediaItemTakeInfo_String(
+        reaper.GetActiveTake(reaper.GetSelectedMediaItem(0, index)), "P_NAME",
+        camera, true)
+end
