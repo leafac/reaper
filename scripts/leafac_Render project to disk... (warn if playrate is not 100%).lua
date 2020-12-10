11b36f841fd9a846fc8e1d6f6aefe0dcfa813a75
Leandro Facchinetti <git@leafac.com>
2020-12-10 04:01:13 +0000

    .

--- scripts/leafac_Render project to disk... (warn if playrate is not 100%).lua
+++
@@ -1,4 +0,0 @@
-if reaper.Master_GetPlayRate(0) ~= 1 and reaper.MB(
-    "The playrate isn’t 100%. Would you like to set it to 100% now to avoid issues in rendering?",
-    "Error", 4) == 6 then reaper.CSurf_OnPlayRateChange(1) end
-reaper.Main_OnCommand(40015, 0)
