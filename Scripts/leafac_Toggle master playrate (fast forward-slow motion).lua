d556f4e3a55e75b256e1d4fbd5bd697d5feca4e9
Leandro Facchinetti <git@leafac.com>
2020-12-24 19:27:18 +0000

    .

--- Scripts/leafac_Toggle master playrate (fast forward-slow motion).lua
+++ Scripts/leafac_Toggle master playrate (fast forward-slow motion).lua
@@ -1,3 +1,4 @@
+-- https://youtu.be/mQUaPRs0KIU
 local playrate = tonumber(string.match(select(2, reaper.get_action_context()),
                                        "(%d+)%%%.lua$")) / 100
 reaper.CSurf_OnPlayRateChange(reaper.Master_GetPlayRate(0) == playrate and 1 or
