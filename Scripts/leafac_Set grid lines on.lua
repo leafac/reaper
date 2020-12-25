396820ee36386e1b9d8fdc7f8ee22a7d685cf2d8
Leandro Facchinetti <git@leafac.com>
2020-12-25 18:09:16 +0000

    .

--- Scripts/leafac_Set grid lines on.lua
+++
@@ -1,4 +0,0 @@
-if reaper.GetToggleCommandStateEx(0, 40145) ==
-    (string.match(select(2, reaper.get_action_context()), "on%.lua$") and 0 or 1) then
-    reaper.Main_OnCommand(40145, 0) -- Options: Toggle grid lines
-end
