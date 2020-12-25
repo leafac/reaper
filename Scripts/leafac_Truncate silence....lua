f7a080853675dfa2b5f092a636849f6a1ae874f2
Leandro Facchinetti <git@leafac.com>
2020-12-25 21:41:35 +0000

    .

--- Scripts/leafac_Truncate silence....lua
+++ Scripts/leafac_Truncate silence....lua
@@ -1,13 +1,38 @@
+-- https://youtu.be/O8o8z48rBbA
 local actionName = string.match(select(2, reaper.get_action_context()),
                                 "leafac_(.+)%.lua$")
+local shouldUndo =
+    string.match(actionName, "Prepare selected items for editing") or
+        string.match(actionName, "Truncate silence in selected items%.%.%.") or
+        string.match(actionName,
+                     "Truncate silence in selected items %(options%)%.%.%.")
+
+local function run(description, identifier)
+    local identifierNumber = type(identifier) == "number" and identifier or
+                                 reaper.NamedCommandLookup(identifier, 0)
+    if identifierNumber == 0 then
+        error(
+            "Command not found: “" .. description .. "” (" .. identifier ..
+                "). For this script to work you must install SWS and all the packages from the default ReaPack repositories. If you already did that, then this is a bug; please report it to reaper@leafac.com or at https://github.com/leafac/reaper/issues/new.")
+    end
+    reaper.Main_OnCommand(identifierNumber, 0)
+end
 
 if reaper.CountSelectedMediaItems(0) == 0 then
     return reaper.MB("No items selected", "Error", 0)
 end
 
+if string.match(actionName, "Prepare selected items for editing") then
+    run("SWS/BR: Normalize loudness of selected items to -23 LUFS",
+        "_BR_NORMALIZE_LOUDNESS_ITEMS23")
+end
+
 reaper.Undo_BeginBlock()
 
-reaper.Main_OnCommand(40315, 0) -- Item: Auto trim/split items (remove silence)...
+if not string.match(actionName,
+                    "Remove gaps between selected items %(Reposition selected items across tracks%)") then
+    run("Item: Auto trim/split items (remove silence)...", 40315)
+end
 
 local items = {}
 local minimumStart = math.huge
@@ -38,11 +63,13 @@ for item in pairs(items) do
     gaps = newGaps
 end
 
-reaper.Undo_EndBlock(actionName .. " - WORK IN PROGRESS", -1)
-reaper.Undo_DoUndo2(0)
+if shouldUndo then
+    reaper.Undo_EndBlock(actionName .. " - WORK IN PROGRESS", -1)
+    reaper.Undo_DoUndo2(0)
+end
 
 local userInputs = ""
-if string.match(actionName, "%(options%)%.%.%.$") then
+if string.match(actionName, "%(options%)") then
     local continue
     continue, userInputs = reaper.GetUserInputs(actionName, 3,
                                                 "Keep (%) (default: 0),Minimum (ms) (default: 0),Maximum (ms) (default: ∞)",
@@ -54,7 +81,7 @@ local keepPercentage = tonumber(userInputsIterator() or nil) or 0
 local keepMinimum = tonumber(userInputsIterator() or nil) or 0
 local keepMaximum = tonumber(userInputsIterator() or nil) or math.huge
 
-reaper.Undo_BeginBlock()
+if shouldUndo then reaper.Undo_BeginBlock() end
 
 for _, gap in ipairs(gaps) do
     local keepLength = math.max(keepMinimum / 1000, math.min(keepMaximum / 1000,
@@ -66,7 +93,9 @@ for _, gap in ipairs(gaps) do
     local stop = gap.stop - keepLength / 2
     if start < stop then
         reaper.GetSet_LoopTimeRange(true, false, start, stop, false)
-        reaper.Main_OnCommand(40201, 0) -- Time selection: Remove contents of time selection (moving later items)
+        run(
+            "Time selection: Remove contents of time selection (moving later items)",
+            40201)
     end
 end
 
