3856f4418cefc4504220675e57ce7772ced50194
Leandro Facchinetti <git@leafac.com>
2021-01-17 17:20:16 +0000

    .

---
+++ Scripts/leafac_Multicam - Insert camera switch marker at current position.lua
@@ -0,0 +1,7 @@
+reaper.AddProjectMarker(0, false,
+                        reaper.GetPlayState() & 1 == 1 and
+                            reaper.GetPlayPosition() or
+                            reaper.GetCursorPosition(), 0,
+                        "Multicam - Switch to camera " ..
+                            string.match(select(2, reaper.get_action_context()),
+                                         "leafac_.*(%d+).*%.lua$"), -1)
